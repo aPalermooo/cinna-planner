b9addb76bc508cb8b0ae0764c52504bfee0b72a0
aPalermooo <ajp2s@login.missouristate.edu>
2025-11-23 14:33:23 -0600

    re-init

--- django-back/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from operator import index
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name='index')
-]
--- spring-back/cabinet/src/main/java/com/cinnamon/cabinet/domain/util/Address.java
+++
@@ -1,9 +0,0 @@
-package com.cinnamon.cabinet.domain.util;
-
-public record Address (
-        String street,
-        String city,
-        String state,
-        String country,
-        String zip
-){}
